1f33a18220e2ef5cbe4fde130e63346ead05bf28
SwetaBhadani <shwetavadani92@gmail.com>
2021-08-01 21:43:36 +0100

    Mavenised and added basic project structure

---
+++ src/test/java/stepDefinition/Hook.java
@@ -0,0 +1,19 @@
+package stepDefinition;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hook {
+
+    @Before
+    public void InitializeTest(Scenario scenario) {
+
+    }
+
+
+    @After
+    public void TearDownTest(Scenario scenario) {
+
+    }
+}
