ac171c2d35b444cc7d5184c00d64f888a26a1b7b
SwetaBhadani <shwetavadani92@gmail.com>
2021-08-01 21:55:15 +0100

    Add create user scenario and corresponding step definition skeleton

--- src/test/java/stepDefinition/UserAPIStepDefinition.java
+++ src/test/java/stepDefinition/UserAPIStepDefinition.java
@@ -1,4 +1,40 @@
 package stepDefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class UserAPIStepDefinition {
+
+    @Given("I have the endpoint to create a user")
+    public void i_have_the_endpoint_to_create_a_user() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I hit a request with below details to create a user")
+    public void i_hit_a_request_with_below_details_to_create_a_user(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I verify the creation of user from the response")
+    public void i_verify_the_creation_of_user_from_the_response() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I verify the status code is equal to {int}")
+    public void i_verify_the_status_code_is_equal_to(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
 }
---
+++ src/test/resources/ReqResUserAPI.feature
@@ -0,0 +1,9 @@
+Feature: Tests ReqRes User API
+
+  Scenario: Verify the creation of a user
+    Given I have the endpoint to create a user
+    When I hit a request with below details to create a user
+      | name     | job    |
+      | morpheus | leader |
+    Then I verify the creation of user from the response
+    And I verify the status code is equal to 200
